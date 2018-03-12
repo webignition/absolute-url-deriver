6bdc68aff925fed2df6d1190f9210b8e3af172f1
Jon Cram <jon@webignition.net>
2018-03-12 15:56:51 +0000

    Coverage++, simplify tests

--- src/AbsoluteUrlDeriver.php
+++ src/AbsoluteUrlDeriver.php
@@ -2,246 +2,160 @@
 
 namespace webignition\AbsoluteUrlDeriver;
 
-/**
- * 
- * @package webignition\AbsoluteUrlDeriver
- *
- */
-class AbsoluteUrlDeriver {
-    
+use webignition\NormalisedUrl\NormalisedUrl;
+use webignition\NormalisedUrl\Path\Path;
+use webignition\Url\Url;
+
+class AbsoluteUrlDeriver
+{
+    const PORT_HTTPS = 443;
+    const SCHEME_HTTPS = 'https';
+
     /**
-     *
-     * @var \webignition\NormalisedUrl\NormalisedUrl 
+     * @var NormalisedUrl
      */
     private $nonAbsoluteUrl = null;
-    
-    
+
     /**
-     *
-     * @var \webignition\NormalisedUrl\NormalisedUrl 
+     * @var NormalisedUrl
      */
     private $sourceUrl = null;
-    
-    
+
     /**
-     *
-     * @var \webignition\NormalisedUrl\NormalisedUrl 
+     * @var NormalisedUrl
      */
     private $absoluteUrl = null;
-    
-    
+
     /**
-     *
      * @param string $nonAbsoluteUrl
-     * @param string $sourceUrl 
+     * @param string $sourceUrl
      */
-    public function __construct($nonAbsoluteUrl = null, $sourceUrl = null) {
+    public function __construct($nonAbsoluteUrl = null, $sourceUrl = null)
+    {
         if (!is_null($nonAbsoluteUrl) && !is_null($sourceUrl)) {
             $this->init($nonAbsoluteUrl, $sourceUrl);
         }
     }
-    
-    
+
     /**
-     * 
      * @param string $nonAbsoluteUrl
      * @param string $sourceUrl
      */
-    public function init($nonAbsoluteUrl, $sourceUrl) {        
-        $this->nonAbsoluteUrl = (trim($nonAbsoluteUrl) == '') 
-                ? new \webignition\Url\Url($sourceUrl) 
-                : new \webignition\Url\Url($nonAbsoluteUrl);
-        
-        $this->sourceUrl = new \webignition\NormalisedUrl\NormalisedUrl($sourceUrl);  
+    public function init($nonAbsoluteUrl, $sourceUrl)
+    {
+        $this->nonAbsoluteUrl = (trim($nonAbsoluteUrl) == '')
+                ? new Url($sourceUrl)
+                : new Url($nonAbsoluteUrl);
+
+        $this->sourceUrl = new NormalisedUrl($sourceUrl);
         $this->absoluteUrl = null;
+
+        $this->deriveAbsoluteUrl();
     }
-    
-    
+
     /**
-     * 
-     * @return \webignition\Url\Url
+     * @return Url
      */
-    public function getAbsoluteUrl() {
-        if (is_null($this->absoluteUrl)) {
-            $this->deriveAbsoluteUrl();            
-        }
-        
+    public function getAbsoluteUrl()
+    {
         return $this->absoluteUrl;
     }
-    
-    
-    private function deriveAbsoluteUrl() {        
+
+    private function deriveAbsoluteUrl()
+    {
         $this->absoluteUrl = clone $this->nonAbsoluteUrl;
 
-        if (!$this->absoluteUrl->isAbsolute()) {        
+        if (!$this->absoluteUrl->isAbsolute()) {
             if ($this->absoluteUrl->isProtocolRelative()) {
                 $this->deriveScheme();
             } else {
-                $this->derivePath();        
+                $this->derivePath();
                 $this->deriveHost();
                 $this->derivePort();
                 $this->deriveScheme();
 
                 $this->deriveUser();
-                $this->derivePass();                
+                $this->derivePass();
             }
         }
     }
-    
-    private function deriveHost() {
+
+    private function deriveHost()
+    {
         if (!$this->absoluteUrl->hasHost()) {
             if ($this->sourceUrl->hasHost()) {
                 $this->absoluteUrl->setHost($this->sourceUrl->getHost());
             }
-        }        
+        }
     }
 
-    private function derivePort() {
+    private function derivePort()
+    {
         if (!$this->absoluteUrl->hasPort()) {
             if ($this->sourceUrl->hasPort()) {
-                $scheme = null;
-                if ($this->sourceUrl->hasScheme()) {
-                    $scheme = $this->sourceUrl->getScheme();
-                }
+                $scheme = $this->sourceUrl->hasScheme()
+                    ? $this->sourceUrl->getScheme()
+                    : null;
+
                 $port = $this->sourceUrl->getPort();
-                // Note: the Url class seems to add the port specification even 
-                // when the standard HTTPS port is used. So in order for the 
-                // tests (which test "expected" behavior) to pass, don't set the 
-                // port if it is using the standard HTTPS port. However, if the 
-                // scheme is non-https and port 443 is specified, then add the 
-                // port anyway.
-                if ($port != 443 || $scheme != 'https') {
+
+                // Apply port only if not https:443
+                if ($port != self::PORT_HTTPS || $scheme != self::SCHEME_HTTPS) {
                     $this->absoluteUrl->setPort($port);
                 }
             }
         }
-    }    
-    
-    private function deriveScheme() {
+    }
+
+    private function deriveScheme()
+    {
         if (!$this->absoluteUrl->hasScheme()) {
             if ($this->sourceUrl->hasScheme()) {
                 $this->absoluteUrl->setScheme($this->sourceUrl->getScheme());
             }
         }
-    }   
-    
-    private function derivePath() {        
-        if ($this->absoluteUrl->hasPath() && $this->absoluteUrl->getPath()->isRelative()) {            
+    }
+
+    private function derivePath()
+    {
+        if ($this->absoluteUrl->hasPath() && $this->absoluteUrl->getPath()->isRelative()) {
             if ($this->sourceUrl->hasPath()) {
-                /* @var $pathDirectory \webignition\NormalisedUrl\Path\Path */
-                $rawPathDirectory = $this->sourceUrl->getPath()->hasFilename() ? dirname($this->sourceUrl->getPath()) . '/': (string)$this->sourceUrl->getPath();
-     
-                $pathDirectory = new \webignition\NormalisedUrl\Path\Path($rawPathDirectory);                                  
-                $derivedPath = $pathDirectory;               
-                
+                /* @var $pathDirectory Path */
+                $rawPathDirectory = $this->sourceUrl->getPath()->hasFilename()
+                    ? dirname($this->sourceUrl->getPath()) . '/'
+                    : (string)$this->sourceUrl->getPath();
+
+                $pathDirectory = new Path($rawPathDirectory);
+                $derivedPath = $pathDirectory;
+
                 if (!$pathDirectory->hasTrailingSlash()) {
                     $derivedPath .= '/../';
                 }
-                
-                $derivedPath .= $this->absoluteUrl->getPath();                
-                $normalisedDerivedPath = new \webignition\NormalisedUrl\Path\Path((string)$derivedPath);                  
+
+                $derivedPath .= $this->absoluteUrl->getPath();
+                $normalisedDerivedPath = new Path((string)$derivedPath);
                 $this->absoluteUrl->setPath($normalisedDerivedPath);
             }
         }
-        
+
         if (!$this->absoluteUrl->hasPath()) {
-            if ($this->sourceUrl->hasPath()) {                
+            if ($this->sourceUrl->hasPath()) {
                 $this->absoluteUrl->setPath($this->sourceUrl->getPath());
             }
-        }       
+        }
     }
-    
-    private function deriveUser() {
+
+    private function deriveUser()
+    {
         if (!$this->absoluteUrl->hasUser() && $this->sourceUrl->hasUser()) {
             $this->absoluteUrl->setUser($this->sourceUrl->getUser());
         }
     }
-    
-    
-    private function derivePass() {
+
+    private function derivePass()
+    {
         if (!$this->absoluteUrl->hasPass() && $this->sourceUrl->hasPass()) {
             $this->absoluteUrl->setPass($this->sourceUrl->getPass());
         }
     }
-    
-    
-    
-//    /**
-//     *
-//     * @return string
-//     */
-//    public function getUrl() {
-//        $url = $this->getScheme().'://'.$this->getCredentialsString().$this->getHost();
-//        
-//        if (!$this->hostEndsWithPathPartSeparator() && !$this->pathStartsWithPathPartSeparator()) {
-//            $url .= '/';
-//        }        
-//        
-//        $url .= $this->getPath().$this->getQueryString();
-//        
-//        return $url;
-//    }    
-//
-//    
-//    /**
-//     *
-//     * @return string
-//     */    
-//    public function getScheme() {
-//        return (parent::getScheme() == '') ? $this->sourceUrl->getScheme() : parent::getScheme();
-//    }
-//    
-//    /**
-//     *
-//     * @return string
-//     */    
-//    public function getHost() {
-//        return (parent::getHost() == '') ? $this->sourceUrl->getHost() : parent::getHost();
-//    }
-//    
-//    /**
-//     *
-//     * @return string
-//     */    
-//    public function getUsername() {
-//        return (parent::getUsername() == '') ? $this->sourceUrl->getUsername() : parent::getUsername();
-//    }
-//    
-//    /**
-//     *
-//     * @return string
-//     */    
-//    public function getPassword() {
-//        return (parent::getPassword() == '') ? $this->sourceUrl->getPassword() : parent::getPassword();
-//    }
-//    
-//    /**
-//     *
-//     * @return string
-//     */    
-//    public function getPath() {                
-//        if ($this->parentPathStartsWithPathPartSeparator()) {
-//            return (parent::getPath() == '') ? $this->sourceUrl->getPath() : parent::getPath();
-//        }
-//  
-//        return ($this->sourceUrl->pathEndsWithPathPartSeparator()) ?
-//            $this->sourceUrl->getPath() . parent::getPath():
-//            $this->sourceUrl->getPath() . '/' . parent::getPath();        
-//    }
-//    
-//    /**
-//     *
-//     * @return string
-//     */    
-//    public function getFragment() {
-//        return (parent::getFragment() == '') ? $this->sourceUrl->getFragment() : parent::getFragment();
-//    }
-//    
-//    /**
-//     *
-//     * @return boolean
-//     */
-//    protected function parentPathStartsWithPathPartSeparator() {
-//        return substr(parent::getPath(), 0, 1) == self::PATH_PART_SEPARATOR;
-//    }
 }
---
+++ tests/AbsoluteUrlDeriverTest.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace webignition\Tests\AbsoluteUrlDeriver;
+
+use webignition\AbsoluteUrlDeriver\AbsoluteUrlDeriver;
+use webignition\Url\Url;
+
+class AbsoluteUrlDeriverTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetAbsoluteUrlEmptyInput()
+    {
+        $absoluteUrlDeriver = new AbsoluteUrlDeriver();
+
+        $this->assertNull($absoluteUrlDeriver->getAbsoluteUrl());
+    }
+
+    /**
+     * @dataProvider getAbsoluteUrlDataProvider
+     *
+     * @param string $nonAbsoluteUrl
+     * @param string $sourceUrl
+     * @param string $expectedAbsoluteUrl
+     */
+    public function testGetAbsoluteUrl($nonAbsoluteUrl, $sourceUrl, $expectedAbsoluteUrl)
+    {
+        $absoluteUrlDeriver = new AbsoluteUrlDeriver($nonAbsoluteUrl, $sourceUrl);
+
+        $absoluteUrl = $absoluteUrlDeriver->getAbsoluteUrl();
+
+        $this->assertInstanceOf(Url::class, $absoluteUrl);
+        $this->assertEquals($expectedAbsoluteUrl, (string)$absoluteUrl);
+    }
+
+    /**
+     * @return array
+     */
+    public function getAbsoluteUrlDataProvider()
+    {
+        return [
+            'non-absolute url is empty' => [
+                'nonAbsoluteUrl' => '',
+                'sourceUrl' => 'http://example.com/foo/bar',
+                'expectedAbsoluteUrl' => 'http://example.com/foo/bar',
+            ],
+            'absolute url; identical to source' => [
+                'nonAbsoluteUrl' => 'http://example.com/',
+                'sourceUrl' => 'http://example.com/',
+                'expectedAbsoluteUrl' => 'http://example.com/',
+            ],
+            'absolute url; different host' => [
+                'nonAbsoluteUrl' => 'http://foo.example.com/',
+                'sourceUrl' => 'http://example.com/',
+                'expectedAbsoluteUrl' => 'http://foo.example.com/',
+            ],
+            'protocol-relative; http' => [
+                'nonAbsoluteUrl' => '//foo.example.com/',
+                'sourceUrl' => 'http://example.com/',
+                'expectedAbsoluteUrl' => 'http://foo.example.com/',
+            ],
+            'protocol-relative; https' => [
+                'nonAbsoluteUrl' => '//foo.example.com/',
+                'sourceUrl' => 'https://example.com/',
+                'expectedAbsoluteUrl' => 'https://foo.example.com/',
+            ],
+            'protocol-relative; source path is ignored' => [
+                'nonAbsoluteUrl' => '//foo.example.com/',
+                'sourceUrl' => 'https://example.com/bar/',
+                'expectedAbsoluteUrl' => 'https://foo.example.com/',
+            ],
+            'hash only' => [
+                'nonAbsoluteUrl' => '#',
+                'sourceUrl' => 'http://example.com/',
+                'expectedAbsoluteUrl' => 'http://example.com/#',
+            ],
+            'hash and identifier' => [
+                'nonAbsoluteUrl' => '#bar',
+                'sourceUrl' => 'http://example.com/',
+                'expectedAbsoluteUrl' => 'http://example.com/#bar',
+            ],
+            'absolute path; no query string' => [
+                'nonAbsoluteUrl' => '/foo',
+                'sourceUrl' => 'http://example.com/one/two/three',
+                'expectedAbsoluteUrl' => 'http://example.com/foo',
+            ],
+            'absolute path; has query string' => [
+                'nonAbsoluteUrl' => '/foo?key=value',
+                'sourceUrl' => 'http://example.com/one/two/three',
+                'expectedAbsoluteUrl' => 'http://example.com/foo?key=value',
+            ],
+            'relative path; no trailing slash on source' => [
+                'nonAbsoluteUrl' => 'file.html',
+                'sourceUrl' => 'http://example.com/path1/path2',
+                'expectedAbsoluteUrl' => 'http://example.com/path1/file.html',
+            ],
+            'relative path; has trailing slash on source' => [
+                'nonAbsoluteUrl' => 'file.html',
+                'sourceUrl' => 'http://example.com/path1/path2/',
+                'expectedAbsoluteUrl' => 'http://example.com/path1/path2/file.html',
+            ],
+            'relative path; non-absolute url has leading double dot, source has trailing slash' => [
+                'nonAbsoluteUrl' => '../file.html',
+                'sourceUrl' => 'http://example.com/path1/path2/',
+                'expectedAbsoluteUrl' => 'http://example.com/path1/file.html',
+            ],
+            'relative path; non-absolute url has leading double dot, source not has trailing slash' => [
+                'nonAbsoluteUrl' => '../file.html',
+                'sourceUrl' => 'http://example.com/path1/path2',
+                'expectedAbsoluteUrl' => 'http://example.com/file.html',
+            ],
+            'relative path; non-absolute url has leading single dot, source has trailing slash' => [
+                'nonAbsoluteUrl' => './file.html',
+                'sourceUrl' => 'http://example.com/path1/path2/',
+                'expectedAbsoluteUrl' => 'http://example.com/path1/path2/file.html',
+            ],
+            'relative path; non-absolute url has leading single dot, source not has trailing slash' => [
+                'nonAbsoluteUrl' => './file.html',
+                'sourceUrl' => 'http://example.com/path1/path2',
+                'expectedAbsoluteUrl' => 'http://example.com/path1/file.html',
+            ],
+            'relative path; source not has path' => [
+                'nonAbsoluteUrl' => '/file.html',
+                'sourceUrl' => 'http://example.com',
+                'expectedAbsoluteUrl' => 'http://example.com/file.html',
+            ],
+            'relative path; source not has path, user' => [
+                'nonAbsoluteUrl' => '/file.html',
+                'sourceUrl' => 'http://user@example.com',
+                'expectedAbsoluteUrl' => 'http://user@example.com/file.html',
+            ],
+            'relative path; source not has path, user, empty pass' => [
+                'nonAbsoluteUrl' => '/file.html',
+                'sourceUrl' => 'http://user:@example.com',
+                'expectedAbsoluteUrl' => 'http://user:@example.com/file.html',
+            ],
+            'relative path; source not has path, user, pass' => [
+                'nonAbsoluteUrl' => '/file.html',
+                'sourceUrl' => 'http://user:pass@example.com',
+                'expectedAbsoluteUrl' => 'http://user:pass@example.com/file.html',
+            ],
+            'port; non-standard http' => [
+                'nonAbsoluteUrl' => '/file.html',
+                'sourceUrl' => 'http://example.com:8080',
+                'expectedAbsoluteUrl' => 'http://example.com:8080/file.html',
+            ],
+            'port; non-standard https' => [
+                'nonAbsoluteUrl' => '/file.html',
+                'sourceUrl' => 'https://example.com:8443',
+                'expectedAbsoluteUrl' => 'https://example.com:8443/file.html',
+            ],
+            'port; standard http' => [
+                'nonAbsoluteUrl' => '/file.html',
+                'sourceUrl' => 'http://example.com:80',
+                'expectedAbsoluteUrl' => 'http://example.com/file.html',
+            ],
+            'port; standard https' => [
+                'nonAbsoluteUrl' => '/file.html',
+                'sourceUrl' => 'https://example.com:443',
+                'expectedAbsoluteUrl' => 'https://example.com/file.html',
+            ],
+            'port; source uses https port for http' => [
+                'nonAbsoluteUrl' => '/file.html',
+                'sourceUrl' => 'http://example.com:443',
+                'expectedAbsoluteUrl' => 'http://example.com:443/file.html',
+            ],
+            'port; source uses http port for https' => [
+                'nonAbsoluteUrl' => '/file.html',
+                'sourceUrl' => 'https://example.com:80',
+                'expectedAbsoluteUrl' => 'https://example.com:80/file.html',
+            ],
+        ];
+    }
+}
