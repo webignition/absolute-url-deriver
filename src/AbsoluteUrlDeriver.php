ffdbc4e1861f64908284306dcd2b0e1479d69fc5
Jon Cram <jon@webignition.net>
2019-01-08 19:55:23 +0000

    Tidy up

--- src/AbsoluteUrlDeriver.php
+++ src/AbsoluteUrlDeriver.php
@@ -30,73 +30,46 @@ class AbsoluteUrlDeriver
             $isProtocolRelative = empty($absolute->getScheme()) && !empty($absolute->getHost());
 
             if ($isProtocolRelative) {
-                $absolute = $this->deriveScheme($base, $absolute);
+                $absolute = $absolute->withScheme($base->getScheme());
             } else {
-                $absolute = $this->derivePath($base, $absolute);
-                $absolute = $this->deriveHost($base, $absolute);
-                $absolute = $this->derivePort($base, $absolute);
-                $absolute = $this->deriveScheme($base, $absolute);
-                $absolute = $this->deriveUserInfo($base, $absolute);
+                if (empty($absolute->getScheme())) {
+                    $absolute = $absolute->withScheme($base->getScheme());
+                }
+
+                if (empty($absolute->getHost())) {
+                    $absolute = $absolute->withHost($base->getHost());
+                }
+
+                if (empty($absolute->getPort())) {
+                    $absolute = $absolute->withPort($base->getPort());
+                }
+
+                if (empty($relative->getPath())) {
+                    $absolute = $absolute->withPath($base->getPath());
+                } else {
+                    $absolute = $this->derivePath($base, $absolute);
+                }
+
+                if (empty($absolute->getUserInfo())) {
+                    $absolute = $absolute->withUserInfo($base->getUserInfo());
+                }
             }
         }
 
         return Normalizer::normalize($absolute);
     }
 
-    private function deriveHost(UriInterface $base, UriInterface $relative): UriInterface
-    {
-        if (empty($relative->getHost())) {
-            if (!empty($base->getHost())) {
-                $relative = $relative->withHost($base->getHost());
-            }
-        }
-
-        return $relative;
-    }
-
-    private function derivePort(UriInterface $base, UriInterface $relative): UriInterface
-    {
-        if (empty($relative->getPort())) {
-            if (!empty($base->getPort())) {
-                $scheme = $base->getScheme() ?? null;
-                $port = $base->getPort();
-
-                // Apply port only if not https:443
-                if ($port != self::PORT_HTTPS || $scheme != self::SCHEME_HTTPS) {
-                    $relative = $relative->withPort($port);
-                }
-            }
-        }
-
-        return $relative;
-    }
-
-    private function deriveScheme(UriInterface $base, UriInterface $relative): UriInterface
-    {
-        if (empty($relative->getScheme())) {
-            if (!empty($base->getScheme())) {
-                return $relative->withScheme($base->getScheme());
-            }
-        }
-
-        return $relative;
-    }
-
     private function derivePath(UriInterface $base, UriInterface $relative): UriInterface
     {
         $relativeUrlPath = new Path($relative->getPath());
-        $baseUrlPath = new Path($base->getPath());
 
         if ($relativeUrlPath->isRelative()) {
-            if (!empty($base->getPath())) {
-                $rawPathDirectory = $baseUrlPath->hasFilename()
-                    ? $baseUrlPath->getDirectory()
-                    : $base->getPath();
+            $basePath = $base->getPath();
 
-                $pathDirectory = new Path($rawPathDirectory);
-                $derivedPath = $pathDirectory;
+            if (!empty($basePath)) {
+                $derivedPath = $basePath;
 
-                if (!$pathDirectory->hasTrailingSlash()) {
+                if ('/' !== $derivedPath[-1]) {
                     $derivedPath .= '/../';
                 }
 
@@ -104,21 +77,6 @@ class AbsoluteUrlDeriver
 
                 $relative = $relative->withPath($derivedPath);
             }
-        }
-
-        if (empty($relative->getPath())) {
-            if (!empty($base->getPath())) {
-                $relative = $relative->withPath($base->getPath());
-            }
-        }
-
-        return $relative;
-    }
-
-    private function deriveUserInfo(UriInterface $base, UriInterface $relative): UriInterface
-    {
-        if (empty($relative->getUserInfo()) && !empty($base->getUserInfo())) {
-            $relative = $relative->withUserInfo($base->getUserInfo());
         }
 
         return $relative;
